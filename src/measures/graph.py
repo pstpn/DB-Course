fe795ef7577e103a9b96038ac9cfd086181328dc
pstpn <stepaha78@gmail.com>
2024-06-04 19:02:23 +0300

    [*]: fix vkr.exe

--- src/measures/graph.py
+++ src/measures/graph.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
+from scipy.interpolate import interp1d
 
 
 class Graph:
@@ -22,20 +23,27 @@ class Graph:
 
     def buildGraph(self, filename, ylabel):
         with PdfPages(f'./{filename}.pdf') as pdf:
+            f_m = interp1d(self.sizes, self.mongo, kind='cubic')
+            f_p = interp1d(self.sizes, self.postgres, kind='cubic')
+            xnew = np.linspace(self.sizes.min(), self.sizes.max(), num=100)
+
             fig = plt.figure(figsize=(12, 8))
             splt = fig.add_subplot()
             plt.xlabel("Размер изображения (Кбайт)")
             plt.ylabel(ylabel)
 
-            custom_lines = [Line2D([0], [0], color="red", lw=2, marker="x", linestyle="--",  markersize=10),
-                            Line2D([0], [0], color="green", lw=2, marker="o", linestyle="solid",  markersize=10)]
+            custom_lines = [Line2D([0], [0], color="red", lw=2, marker="x", linestyle="--", markersize=10),
+                            Line2D([0], [0], color="green", lw=2, marker="o", linestyle="solid", markersize=10)]
 
             plt.legend(custom_lines, ['PostgreSQL', 'MongoDB'])
             # plt.semilogx()
             plt.semilogy()
 
-            splt.plot(self.sizes, self.postgres, color="red", marker="x", linestyle="--", linewidth=2, markersize=10)
-            splt.plot(self.sizes, self.mongo, color="green", marker="o", linestyle="solid", linewidth=2, markersize=10)
+            splt.plot(xnew, f_p(xnew), color="red", linestyle="--", linewidth=2)
+            splt.plot(xnew, f_m(xnew), color="green", linestyle="solid", linewidth=2)
+            splt.plot(self.sizes, self.postgres, color="red", marker="x", linestyle='none', markersize=10)
+            splt.plot(self.sizes, self.mongo, color="green", marker="o", linestyle='none', markersize=10)
+
             splt.grid(True, linestyle='--', linewidth=0.5, color='gray')
             splt.grid(True)
 
@@ -45,7 +53,6 @@ class Graph:
 
 
 if __name__ == "__main__":
-
     files = [
         'result1.csv',
         'result2.csv',
